66b1b7bc15280af534861bd341da56967cedae8d
Mochi <152498304+heimochi@users.noreply.github.com>
2025-08-06 13:28:30 +0200

    remove sensitive data, convert source code to r

--- cope/scripts/shared/utilities.r
+++ cope/scripts/shared/utilities.r
@@ -7,16 +7,26 @@ editor: visual
 
 ## Shared functions
 
-Counts 
+## Function for patient count at each measurement 
+
 ```{r}
 summarize_patient_counts <- function(dataset) {
-  dataset %>%
+  
+  # Summarize the count of patients for each assessment context label
+  patient_counts <- dataset %>%
     group_by(assessment_context_label) %>%
     summarize(
       patient_count = n_distinct(respondent_id),
-      .groups = "drop"
+      .groups = 'drop' # To ungroup after summarization
     )
+  
+  return(patient_counts)
 }
+
+# Example usage
+# counts <- summarize_patient_counts(your_dataset)
+# print(counts)
+
 ```
 
 
